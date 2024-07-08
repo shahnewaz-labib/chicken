10badc3a80763fbf691cc2814e473f61ee3ff535
shahnewaz-labib <shahnewaz@iut-dhaka.edu>
2024-07-08 13:32:46 +0600

    changed styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
+'use client';
+
 const Home = () => {
-  return <div>Hello</div>;
+  return (
+    <div className="mt-8 justify-center flex">
+      <div className="justify-between">
+        <input className="m-2 bg-slate-200 h-10 px-2 rounded" type="text" />
+        <button className="ml-4 bg-blue-500 hover:bg-blue-700 text-white font-medium py-2 px-4 rounded">Add Todo</button>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
